553586234fb36fd9dff26f6cd5591747a29c9c6f
kaushal <kaushalkishorroy21@gmail.com>
2024-06-22 23:21:28 +0530

    input from

--- blog/src/components/Signup.jsx
+++ blog/src/components/Signup.jsx
@@ -38,19 +38,57 @@ function Signup() {
           </span>
         </div>
         <h2 className="text-center text-2xl font-bold leading-tight ">
-            Sign up to create account
+          Sign up to create account
         </h2>
         <p className="mt-2 text-center text-base text-black/60">
-        Already have a account
-        <Link 
-        to="/login"
-        className="font-medium text-primary transition-all duration-200
-        hover:underline">
+          Already have a account
+          <Link
+            to="/login"
+            className="font-medium text-primary transition-all duration-200
+        hover:underline"
+          >
             Sign In
-        </Link>
+          </Link>
         </p>
-        {error && <p className="text-red-600 mt-8 text-center">
-            {error}</p>}
+        {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+
+        <form onSubmit={handleSubmit(create)}>
+          <div className="space-y-5">
+            <Input
+              label="Full Name"
+              placeholder="Enter your full name"
+              {...register("name", {
+                required: true,
+              })}
+            />
+
+            <Input
+              label="Email"
+              type="email"
+              placeholder="Enter  ypur email"
+              {...register("email", {
+                required: true,
+                validate: {
+                  matchPatern: (value) =>
+                    /^([A-Z|a-z|0-9](\.|_){0,1})+[A-Z|a-z|0-9]\@([A-Z|a-z|0-9])+((\.){0,1}[A-Z|a-z|0-9]){2}\.[a-z]{2,3}$/gm ||
+                    "Email addrsss must be a valid",
+                },
+              })}
+            />
+
+            <Input
+            label="password"
+            type="password"
+            placeholder="Enter your password"
+             {...register("password",{
+              required:true,
+             })}
+            />
+
+            <Button type="submit" className="w-full">Signup</Button>
+          </div>
+
+        </form>
       </div>
     </div>
   );
