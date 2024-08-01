0892709cd17c773162a68a9febdb95a175e247d6
kaushal <kaushalkishorroy21@gmail.com>
2024-08-01 22:13:01 +0530

    add

--- blog/src/components/footer/footer.jsx
+++ blog/src/components/footer/footer.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='bg-slate-900 text-white w-ful h-[200px] '>footer</div>
+    <div className='bg-slate-900 text-white w-ful h-[200px] '>
+      <div>
+        Footer
+      </div>
+    </div>
   )
 }
 
